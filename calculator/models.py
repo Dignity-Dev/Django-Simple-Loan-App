0a9922d3ffbed819612a6c1107756ac8d8c0a445
devfaoed <bashirhaadiy@gmail.com>
2023-06-14 22:28:54 +0100

    Django Simple Loan Application

---
+++ calculator/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Scenario(models.Model):
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    down_payment = models.DecimalField(max_digits=10, decimal_places=2)
+    mortgage_term = models.IntegerField()
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    total_loan_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    monthly_payment = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    total_amount_paid = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    total_interest_paid = models.DecimalField(max_digits=10, decimal_places=2, null=True)
